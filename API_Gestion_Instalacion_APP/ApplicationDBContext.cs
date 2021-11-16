b8222c283d775411af7e580df5fc534241670127
Pablo Gatica <pablomartin.gatica@gmail.com>
2021-11-16 16:40:04 -0300

    Creacion GitIgnore

---
+++ API_Gestion_Instalacion_APP/ApplicationDBContext.cs
@@ -0,0 +1,24 @@
+﻿using API_Gestion_Instalacion_APP.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gestion_Instalacion_APP
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet <Telefonos> Telefono { get; set; }
+        public DbSet<Sensores> Sensor { get; set; }
+        public DbSet<Operarios> Operario { get; set; }
+        public DbSet<Apps> App { get; set; }
+        public DbSet<Instalaciones> Instalacion { get; set; }
+
+
+    }
+}
---
+++ API_Gestion_Instalacion_APP/Controllers/OperariosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gestion_Instalacion_APP.Controllers
+{
+    [Route("api/1.0/[controller]")]
+    [ApiController]
+    public class OperariosController : ControllerBase
+    {
+    }
+}
