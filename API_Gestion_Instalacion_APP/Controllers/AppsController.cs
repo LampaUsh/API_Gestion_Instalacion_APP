879ce90f409525e6e68ef1c009b32ac1d8c25d6b
Pablo Gatica <pablomartin.gatica@gmail.com>
2021-11-16 19:55:45 -0300

    Correccion migracion

--- API_Gestion_Instalacion_APP/Controllers/AppsController.cs
+++ API_Gestion_Instalacion_APP/Controllers/AppsController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API_Gestion_Instalacion_APP.Modelos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +13,26 @@ namespace API_Gestion_Instalacion_APP.Controllers
     [ApiController]
     public class AppsController : ControllerBase
     {
+        private readonly ApplicationDBContext context;
+
+        public AppsController(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Apps>>> Get()
+        {
+            return await context.Apps.ToListAsync();
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult>Post (Apps apps)
+        {
+            context.Add(apps);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
