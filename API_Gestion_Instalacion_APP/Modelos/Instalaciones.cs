c2befed59ab4dee8c756ccb9dac93cb8503aa7e4
Pablo Gatica <pablomartin.gatica@gmail.com>
2021-11-16 14:50:02 -0300

    Creacion de Modelos

---
+++ API_Gestion_Instalacion_APP/Modelos/Instalaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Gestion_Instalacion_APP.Modelos
+{
+    public class Instalaciones
+    {
+        public int Id { get; set; }
+        public Boolean Exitosa { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
